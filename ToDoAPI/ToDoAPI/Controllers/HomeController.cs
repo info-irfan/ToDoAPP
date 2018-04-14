49910ace3f57543eec37f99c4d67cfa9da0776fa
Rajnish Jha <rajha@apttus.com>
2018-04-14 23:31:55 +0530

    Initial Commit

---
+++ ToDoAPI/ToDoAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+
+        public IActionResult Get()
+        {
+            return new JsonResult("Hello, I am here");
+        }
+    }
+}
