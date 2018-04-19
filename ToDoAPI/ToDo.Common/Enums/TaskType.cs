54be623ec1fbf2970668a17ad592f175ce3b55db
irfan <irfan@IRFAN-PC>
2018-04-19 22:47:48 +0530

    1. Added Model classes 2. Added Enums 3.Added DbContext

---
+++ ToDoAPI/ToDo.Common/Enums/TaskType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.Common.Enums
+{
+    public enum TaskTypeEnum
+    {
+        ONCE = 1, DAILY, WEEKLY, MONTHLY, YEARLY, CUSTOM
+    }
+}
---
+++ ToDoAPI/ToDo.DataAccess/Repository/ToDoDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Model.Model;
+
+namespace ToDo.DataAccess.Repository
+{
+    public class ToDoDBContext: DbContext
+    {
+       public ToDoDBContext(DbContextOptions<ToDoDBContext>options):base(options)
+        {
+
+        }
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<TaskReminder> TaskReminder { get; set; }
+    }
+}
