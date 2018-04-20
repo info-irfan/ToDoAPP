6c83236ca00d5baccd63a1c6c817bb2d02d39f5b
irfan <irfan@IRFAN-PC>
2018-04-20 22:09:57 +0530

    Added Repository & UnitOfWork

---
+++ ToDoAPI/ToDo.DataAccess/Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.DataAccess.Repository.Interfaces
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        ToDoDBContext Context { get; }
+        void Commit();
+    }
+}
